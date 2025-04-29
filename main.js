602471aab504216c21824b677b59454ccce727e1
jose.adelson2002 <jose.adelson2002@academico.ufs.br>
2025-04-29 15:56:00 -0300

    function that format displayed number created

--- main.js
+++ main.js
@@ -17,6 +17,29 @@ class Calculator {
     this.clear();
   }
 
+  formatDisplayNumber(number) {
+    const stringNumber = number.toString();
+
+    const integerDigits = parseFloat(stringNumber.split(".")[0]);
+    const decimalDigits = stringNumber.split(".")[1];
+
+    let integerDisplay;
+
+    if (isNaN(integerDigits)) {
+      integerDisplay = "";
+    } else {
+      integerDisplay = integerDigits.toLocaleString("en", {
+        maximumFractionDigits: 0,
+      });
+    }
+
+    if (decimalDigits != null) {
+      return `${integerDisplay}.${decimalDigits}`;
+    } else {
+      return integerDisplay;
+    }
+  }
+
   delete() {
     this.currentOperand = this.currentOperand.toString().slice(0, -1);
   }
@@ -56,6 +79,8 @@ class Calculator {
   }
 
   chooseOperation(operation) {
+    if (this.currentOperand == "") return;
+
     if (this.operation != "") {
       this.calculate();
     }
@@ -78,10 +103,12 @@ class Calculator {
   }
 
   updateDisplay() {
-    this.previousOperandTextElement.innerText = `${this.previousOperand} ${
-      this.operation || ""
-    }`;
-    this.currentOperandTextElement.innerText = this.currentOperand;
+    this.previousOperandTextElement.innerText = `${this.formatDisplayNumber(
+      this.previousOperand
+    )} ${this.operation || ""}`;
+    this.currentOperandTextElement.innerText = this.formatDisplayNumber(
+      this.currentOperand
+    );
   }
 }
 
